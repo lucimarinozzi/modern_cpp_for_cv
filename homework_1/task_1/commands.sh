76d6389496c75a7041559c608ad58520a70e1520
a473563 <a473563@IT000002075926.enelint.global>
2022-05-31 16:15:37 +0200

    added solution for homework_1/task_1

---
+++ homework_1/task_1/commands.sh
@@ -0,0 +1,11 @@
+#1. Count how many lines are there in “data.dat”.
+wc -l < data.dat # --> -l print the newlines count
+
+#2. Count how many lines of those contain “dolor” or “dalor”?
+grep -c d[ao]lor data.dat 
+
+#3. Count how many words are there in “data.dat”?
+wc -w < data.dat
+
+#4. Count how many of those start with “mol”?
+grep -c -o "\bmol\w*" data.dat
